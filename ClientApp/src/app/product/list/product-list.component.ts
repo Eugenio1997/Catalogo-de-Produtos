f33488032b0724731e2b50b71004017dff91bc59
Eugenio Lopes Fernandes Lima <eugeniolopesfernandeslima1997@outlook.com>
2023-09-23 21:00:23 -0300

    Adicionando injecao de dependencia do service e o referenciando com os seus metodos

--- ClientApp/src/app/product/list/product-list.component.ts
+++ ClientApp/src/app/product/list/product-list.component.ts
@@ -1,47 +1,60 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Product} from "../../interfaces";
+import {ProductService} from "../service/product.service";
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  public products: Product[] = [];
-  public pageIndex = 1;
+  public products: Product[] = new Array<Product>()
+  public pageIndex: number = 1;
   public totalPages: number = 1;
   private _baseUrl: string;
   private _http: HttpClient;
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  private _productService: ProductService;
+
+  constructor(http: HttpClient,
+              @Inject('BASE_URL') baseUrl: string,
+              private productService: ProductService) {
     this._http = http;
     this._baseUrl = baseUrl;
+    this._productService = productService;
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.fetchProducts(this.pageIndex);
   }
 
-  public pagePrev(){
+  public pagePrev(): void{
     if(this.pageIndex > 1){
       this.pageIndex = this.pageIndex - 1;
       this.fetchProducts(this.pageIndex);
     }
   }
 
-  public pageNext(){
+  public pageNext(): void{
     if(this.pageIndex < this.totalPages){
       this.pageIndex = this.pageIndex + 1;
       this.fetchProducts(this.pageIndex);
     }
   }
 
-  fetchProducts(pageIndex:number) {
-    this._http.get<Product[]>(this._baseUrl + 'product?pageIndex=' + this.pageIndex, { observe: 'response' })
-      .subscribe((data) => {
+  public fetchProducts(pageIndex:number): void {
+    this.productService.fetchProducts(pageIndex)
+      .subscribe((data:HttpResponse<Array<Product>>): void => {
         this.totalPages = Number(data.headers.get('totalPages'));
         this.products = data.body as Product[];
       });
   }
 
+  public deleteProduct(id: number): void{
+    this.productService.deleteProduct(id)
+      .subscribe((data:HttpResponse<Array<Product>>): void => {
+        this.totalPages = Number(data.headers.get('totalPages'));
+        this.products = data.body as Product[];
+      });
+  }
 }
