1632eddf2099ee3e09d5ed4dfebe4cd1c8c761af
Eugenio Lopes Fernandes Lima <eugeniolimaeugenio5000@outlook.com>
2024-01-08 22:42:09 -0300

    feature/116# Adicionando refresh do token e lancando novamente a requisicao com o token válido

--- ClientApp/src/app/components/authentication/shared/services/auth.service.ts
+++ ClientApp/src/app/components/authentication/shared/services/auth.service.ts
@@ -1,28 +1,53 @@
 import {Inject, Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {FormGroup} from "@angular/forms";
-import {BehaviorSubject, catchError, Observable, tap} from "rxjs";
-import {ResponseSignin} from "@interfaces/authentication/response-signin";
+import {Observable, tap} from "rxjs";
 import {TokenService} from "@components/authentication/shared/services/token.service";
 import {RefreshTokenService} from "@components/authentication/shared/services/refresh-token.service";
-import {SigninService} from "@components/authentication/signin/signin.service";
+import {RequestRefreshToken} from "@interfaces/authentication/refresh-token/request-refresh-token";
+import {ResponseSignin} from "@interfaces/authentication/signin/response-signin";
+
 
 @Injectable()
 export class AuthService {
-
   constructor(private _http: HttpClient,
               @Inject('BACKEND_BASE_URL') private backendBaseUrl: string,
               private router: Router,
               private _tokenService: TokenService,
               private _refreshToken: RefreshTokenService) {}
 
-  public signin(formData: FormGroup): Observable<HttpResponse<ResponseSignin>> {
+
+  public getToken(): string {
+    return this._tokenService.getToken()!;
+  }
+
+  public setToken(accessToken: string) {
+    this._tokenService.setToken(accessToken);
+  }
+
+  public removeToken(): void {
+    this._tokenService.removeToken();
+  }
+
+  public getRefreshToken(): string {
+    return this._refreshToken.getRefreshToken()!;
+  }
+
+  public setRefreshToken(accessToken: string) {
+    this._refreshToken.setRefreshToken(accessToken);
+  }
+
+  public removeRefreshToken(): void {
+    this._refreshToken.removeRefreshToken();
+  }
+
+  public signinEndpointCall(formData: FormGroup): Observable<HttpResponse<ResponseSignin>> {
     return this._http
       .post<any>(this.backendBaseUrl + 'auth/' + 'signin', formData, {observe: 'response'})
       .pipe(
         tap(response => {
-          if(response.status == 200){
+          if (response.status == 200) {
             const accessToken = response.body!.accessToken;
             const refreshToken = response.body!.refreshToken;
             this._tokenService.setToken(accessToken);
@@ -30,6 +55,17 @@ export class AuthService {
           }
         })
       )
+  }
+
+  public refreshTokenEndpointCall(credentials: RequestRefreshToken): any {
+    const body = credentials;
+    const headers = new HttpHeaders();
+    headers.set('Content-Type', 'application/json; charset=utf-8');
+    return this._http
+      .post<any>(this.backendBaseUrl + 'auth/' + 'refresh',
+        body,
+        {observe: 'response', headers})
+
   }
 
 }
