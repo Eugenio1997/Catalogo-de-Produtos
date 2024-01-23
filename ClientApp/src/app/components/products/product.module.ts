3fc6d3af3a78cbafcc98823c164cc37522c8318d
Eugenio Lopes Fernandes Lima <eugeniolimaeugenio5000@outlook.com>
2024-01-23 01:30:37 -0300

    feature/#132 [Frontend] Adicionando o componente e serviço Cart ao módulo de Produto

--- ClientApp/src/app/components/products/product.module.ts
+++ ClientApp/src/app/components/products/product.module.ts
@@ -5,13 +5,15 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 import {SharedModule} from "@shared/shared.module";
-import {ProductService} from "./product.service";
 import {ProductListComponent} from "@components/products/list/product-list.component";
 import {ProductCardComponent} from "@components/products/subcomponents/product-card/product-card.component";
 import {RegisterProductComponent} from "@components/products/register/register-product.component";
 import {CheckProductTypePipe} from "@components/products/subcomponents/product-card/pipes/check-product-type.pipe";
 import {ProductRoutingModule} from "@components/products/product-routing-module";
 import {ProductDetailComponent} from "@components/products/detail/product-detail.component";
+import { CartComponent } from './cart/cart.component';
+import {ProductService} from "@components/products/services/product.service";
+import {CartService} from "@components/products/services/cart.service";
 
 
 @NgModule({
@@ -20,7 +22,8 @@ import {ProductDetailComponent} from "@components/products/detail/product-detail
     ProductCardComponent,
     RegisterProductComponent,
     ProductDetailComponent,
-    CheckProductTypePipe
+    CheckProductTypePipe,
+    CartComponent
   ],
   imports: [
     CommonModule,
@@ -35,11 +38,12 @@ import {ProductDetailComponent} from "@components/products/detail/product-detail
     ProductCardComponent,
     RegisterProductComponent,
     CheckProductTypePipe,
-    ProductDetailComponent
-
+    ProductDetailComponent,
+    CartComponent
   ],
   providers: [
-    ProductService
+    ProductService,
+    CartService
   ],
 })
 export class ProductModule { }
