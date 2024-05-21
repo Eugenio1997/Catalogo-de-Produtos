2739d2ccade5119dff993e3ac95d7a1876fa4a8c
Eugenio Lopes Fernandes Lima <eugeniolimaeugenio5000@outlook.com>
2024-05-21 02:56:17 -0300

    Adicionando botoes de autenticacao e logout

--- ClientApp/src/app/components/shared/header/header.component.ts
+++ ClientApp/src/app/components/shared/header/header.component.ts
@@ -1,6 +1,9 @@
 import {ChangeDetectorRef, Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {Subject} from "rxjs";
 import {CartService} from "@components/checkout/services/cart.service";
+import {TokenService} from "@components/authentication/shared/services/token.service";
+import {AuthService} from "@components/authentication/shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -11,11 +14,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
 
   constructor(private _cartService: CartService,
-              private changeDetector: ChangeDetectorRef) {}
+              private changeDetector: ChangeDetectorRef,
+              private _authService: AuthService,
+              private _router: Router) {}
 
 
 
   //properties
+  public isUserLoggedIn: boolean = false;
+  public loggedInUsername: string = '';
   public notifier = new Subject();
   public totalQuantityOfItemsOnCart: number = 0;
   public isProductDetailOrListingLoaded: boolean = false;
@@ -24,6 +31,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
   public deviceType!: Array<{ deviceType: string; isEnable: boolean }>;
 
   ngOnInit(): void {
+
+    this._authService.isAuthenticated()
+        .subscribe( (isAuthenticated: boolean) => {
+          this.isUserLoggedIn = isAuthenticated;
+        });
+
+    this.loggedInUsername = this._authService.getLoggedInUsername();
+
+
 
     this._cartService
       .getIsCartDisplayedOnHeader()
@@ -40,7 +56,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
       .subscribe( (updatedTotalQuantityOfItems) => {
         this.totalQuantityOfItemsOnCart = updatedTotalQuantityOfItems
       });
-
   }
 
 
@@ -52,6 +67,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
   public sidenavToggle(): void {
     this.menuStatus = !this.menuStatus;
     this.sidenavToggled.emit(this.menuStatus);
+  }
+
+  public logout(){
+    this._authService.removeToken();
+    this._router.navigate(['/products']);
+    this.ngOnInit();
   }
 
 
--- Domain/Models/Response/User/ResponseSigninUserModel.cs
+++ Domain/Models/Response/User/ResponseSigninUserModel.cs
@@ -6,6 +6,7 @@ public class ResponseSigninUserModel
 {
     
     public required int Id { get; set; }
+    public string UserName { get; set; }
     public string AccessToken { get; set; }
     public Guid RefreshToken { get; set; }
     
